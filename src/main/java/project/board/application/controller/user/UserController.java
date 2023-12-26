018a3039a5c290ff5ab76065dfd85d1f273aa8fb
pinetree <eodnjs568@gmail.com>
2023-12-26 16:31:48 +0900

    chore : UserSignUPRequest 내 필드에 대하여 Validation 관련 어노테이션 추가
    - TODO : @Email VS @Pattern(regexp) 직접작성

--- src/main/java/project/board/application/controller/user/UserController.java
+++ src/main/java/project/board/application/controller/user/UserController.java
@@ -1,9 +1,13 @@
 package project.board.application.controller.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.board.application.controller.user.request.UserSignUpRequest;
@@ -20,8 +24,13 @@ public class UserController {
         return "/users/sign-up";
     }
 
-    @PostMapping String signUP(UserSignUpRequest dto, Model model) {
-        User savedUser = userService.save(dto);
+    @PostMapping
+    String signUP(@Valid @ModelAttribute UserSignUpRequest dto, Model model, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return ":/users/sign-up";
+        }
+        User savedUser = userService.createUser(dto);
         model.addAttribute("user", savedUser);
         return "redirect:/";
     }
