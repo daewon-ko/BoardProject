f777f1a7037f6d53890494837b5405fc36bcd170
pinetree <eodnjs568@gmail.com>
2023-12-26 15:34:09 +0900

    chore : User Class 내 필드에 대하여 Jpa Annotation 추가

--- src/main/java/project/board/domain/User.java
+++ src/main/java/project/board/domain/User.java
@@ -1,18 +1,29 @@
 package project.board.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.board.util.BaseTimeEntity;
 
 @Entity
-public class User {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class User extends BaseTimeEntity {
     @Id
     private long id;
+    @Column(name = "name", unique = true)
     private String name;
+    @Column(name = "email")
     private String email;
+    @Column(name = "nickname", unique = true)
     private String nickname;
-
+    @Column(name = "password")
     private String password;
-
+    @Lob
     private String profileImage;
 
 
